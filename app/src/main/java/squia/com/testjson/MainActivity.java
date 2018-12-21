e2b604381dba52cf20a7fbe132241e30aa11532b
AhmedRammadan <43143580+AhmedRammadan@users.noreply.github.com>
2018-12-21 17:39:24 +0200

    upData

--- app/src/main/java/squia/com/testjson/MainActivity.java
+++ app/src/main/java/squia/com/testjson/MainActivity.java
@@ -2,6 +2,7 @@ package squia.com.testjson;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,8 +22,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tv_userId  ,tv_Id , tv_title ,tv_body;
-    String userId  ,Id , title ,body;
+    private TextView tv_userId, tv_Id, tv_title, tv_body;
+    private String userId, Id, title, body;
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,15 +36,22 @@ public class MainActivity extends AppCompatActivity {
         tv_body = findViewById(R.id.body);
 
         //@TODO getData form website jsonplaceholder.typicode.com
-        getData();
+        //getData();
 
         /**
          * TODO  setData form website jsonplaceholder.typicode.com
          * Just Test
          */
-        setData();
+        try {
+            setData();
+        } catch (Exception e) {
+            Log.e(TAG, "Error", e);
+
+        }
+
     }
-    private void getData(){
+
+    private void getData() {
         String url = "https://jsonplaceholder.typicode.com/posts";
         StringRequest request = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
@@ -55,53 +65,61 @@ public class MainActivity extends AppCompatActivity {
                             title = post.getString("title");
                             body = post.getString("body");
                             tv_userId.setText(userId);
-                            tv_Id .setText(Id);
-                            tv_title .setText(title);
-                            tv_body .setText(body);
+                            tv_Id.setText(Id);
+                            tv_title.setText(title);
+                            tv_body.setText(body);
                         } catch (JSONException e) {
                             e.printStackTrace();
-                            Toast.makeText(MainActivity.this,  "Error", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
                         }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
 
-                Toast.makeText(MainActivity.this,  error.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
         Volley.newRequestQueue(this).add(request);
     }
-    private void setData(){
-        String url = "https://jsonplaceholder.typicode.com/posts";
-        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
 
-                Toast.makeText(MainActivity.this,response.toString(), Toast.LENGTH_SHORT).show();
+    private void setData() {
+        String url = "http://water.sitksahost.com/api/register";
+        StringRequest request = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
 
-            }
-        }, new Response.ErrorListener() {
+                        Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+                        Log.d("response", response.toString());
+
+                    }
+                }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.e(TAG, "ErrorResponse", error);
             }
-        }){
+        }) {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
-                HashMap<String , String > map = new HashMap<>();
-                map.put("userId",userId);
-                map.put("Id",Id);
-                map.put("title",title);
-                map.put("body",body);
+                HashMap<String, String> map = new HashMap<>();
+                map.put("firstName", "ahmed");
+                map.put("lastName", "Ramadan");
+                map.put("email", "aaaa@gmail.com");
+                map.put("password", "123456");
+                map.put("addressDetails", "sdasdfad");
+                map.put("area", "ddd");
+                map.put("city", "ddd");
+                map.put("country", "ddd");
+                map.put("state", "ddd");
                 return map;
             }
 
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
-                HashMap<String , String> header = new HashMap<>();
+                HashMap<String, String> header = new HashMap<>();
                 header.put("Content-Type", "application/json");
-                header.put("apiKey", "xxxxxxxxxxxxxxx");
+                header.put("Accept", "application/json");
                 return header;
             }
         };
